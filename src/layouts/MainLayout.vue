46b9a6c3b31b34675df5a12939f0a2f73173211a
Arturo Saldivar <damonbalam@gmail.com>
2023-04-05 02:26:58 -0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-white text-primary">
+        <q-toolbar-title> Natalia Segura </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer class="" style="border-top: 1px solid #fefefe;">
+      <q-tabs
+        v-model="tab"
+        :mobile-arrows="true"
+        class="bg-white text-primary"
+      >
+        <q-tab name="perfil" icon="o_person" label="Perfil" />
+        <q-tab name="alimentos" icon="o_restaurant" label="Alimentos" />
+        <q-tab name="plan" icon="o_format_list_bulleted" label="Plan" />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue'
+
+const essentialLinks: EssentialLinkProps[] = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+const leftDrawerOpen = ref(false)
+const tab = ref('alimentos')
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
